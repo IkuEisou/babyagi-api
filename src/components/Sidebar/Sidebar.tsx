694ee3d537ec86c349b6d737fa006637c9e4501d
iku <ycyu@milize.co.jp>
2024-05-29 12:31:40 +0900

    set the background color of the sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -11,6 +11,7 @@ export const Sidebar: FC<SidebarProps> = ({ onMenuClick }) => {
   return (
     <aside
       className={`fixed bottom-0 top-0 z-50 flex h-full w-64 flex-none flex-col space-y-2 bg-neutral-900 p-2 transition-all sm:relative sm:top-0`}
+      style={{background: "#4968B6"}}
     >
       <SidebarHeader onMenuClick={onMenuClick} />
       <Executions />
