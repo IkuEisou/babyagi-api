723e31c7a294db4a0abb3b74e24f920f9f0e4c06
iku <ycyu@milize.co.jp>
2024-08-01 15:18:33 +0900

    formatted

--- src/lib/agents/babyelfagi/tools/utils/textCompletion.ts
+++ src/lib/agents/babyelfagi/tools/utils/textCompletion.ts
@@ -1,7 +1,7 @@
 import { AgentMessage, AgentTask } from '@/types';
 import { ChatOpenAI } from 'langchain/chat_models/openai';
-import { ChatAnthropic } from "@langchain/anthropic";
-import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { ChatAnthropic } from '@langchain/anthropic';
+import { ChatPromptTemplate } from '@langchain/core/prompts';
 import { textCompletionToolPrompt } from '../../../../../utils/prompt';
 import { HumanChatMessage } from 'langchain/schema';
 
@@ -19,73 +19,75 @@ export const textCompletion = async (
 ) => {
   if (!modelName.includes('claude') && prompt.length > 3200) {
     modelName = 'gpt-3.5-turbo-16k-0613';
+    console.log(`TextCompletion for the ${prompt} by ${modelName} forced! `);
   }
   console.log('textCompletion by ' + modelName);
   try {
-    if (modelName.includes('claude')){
+    if (modelName.includes('claude')) {
       const llm = new ChatAnthropic({
-          anthropicApiKey: process.env['ANTHROPIC_API_KEY'],
-          modelName,
-          temperature: 0.2,
-          maxTokensToSample: 800,
-          topP: 1,
-          streaming: true,
-        });
-      const systemPrompt = textCompletionToolPrompt(objective,language,task.task,dependentTasksOutput);
+        anthropicApiKey: process.env['ANTHROPIC_API_KEY'],
+        modelName,
+        temperature: 0.2,
+        maxTokensToSample: 800,
+        topP: 1,
+        streaming: true,
+      });
+      const systemPrompt = textCompletionToolPrompt(
+        objective,
+        language,
+        task.task,
+        dependentTasksOutput,
+      );
       const prompt4Anthropic = ChatPromptTemplate.fromMessages([
-        [
-          "system",
-          systemPrompt,
-        ],
-        ["human", "{input}"],
+        ['system', systemPrompt],
+        ['human', '{input}'],
       ]);
       const response = await prompt4Anthropic.pipe(llm).invoke({
         input: prompt,
       });
       return response.text;
-    }
-    else{
+    } else {
       const llm = new ChatOpenAI(
+        {
+          openAIApiKey: userApiKey,
+          modelName,
+          temperature: 0.2,
+          maxTokens: 800,
+          topP: 1,
+          frequencyPenalty: 0,
+          presencePenalty: 0,
+          streaming: true,
+          callbacks: [
             {
-              openAIApiKey: userApiKey,
-              modelName,
-              temperature: 0.2,
-              maxTokens: 800,
-              topP: 1,
-              frequencyPenalty: 0,
-              presencePenalty: 0,
-              streaming: true,
-              callbacks: [
-                {
-                  handleLLMNewToken(token: string) {
-                    messageCallnback?.({
-                      content: token,
-                      type: task.skill,
-                      id: id,
-                      taskId: task.id.toString(),
-                      status: 'running',
-                    });
-                  },
-                },
-              ],
+              handleLLMNewToken(token: string) {
+                messageCallnback?.({
+                  content: token,
+                  type: task.skill,
+                  id: id,
+                  taskId: task.id.toString(),
+                  status: 'running',
+                });
+              },
             },
-            { baseOptions: { AbortSignal: signal } },
-          );
+          ],
+        },
+        { baseOptions: { AbortSignal: signal } },
+      );
 
-        const response = await llm.call([new HumanChatMessage(prompt)]);
-        messageCallnback?.({
-          content: response.text,
-          type: task.skill,
-          id: id,
-          taskId: task.id.toString(),
-          status: 'complete',
-        });
+      const response = await llm.call([new HumanChatMessage(prompt)]);
+      messageCallnback?.({
+        content: response.text,
+        type: task.skill,
+        id: id,
+        taskId: task.id.toString(),
+        status: 'complete',
+      });
 
-        if (messageCallnback instanceof AbortSignal) {
-          throw new Error('messageCallnback cannot be of type AbortSignal');
-        }
+      if (messageCallnback instanceof AbortSignal) {
+        throw new Error('messageCallnback cannot be of type AbortSignal');
+      }
 
-        return response.text;
+      return response.text;
     }
   } catch (error: any) {
     if (error.name === 'AbortError') {
